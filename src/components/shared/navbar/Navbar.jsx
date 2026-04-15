c10186c3c9b5345c871709237e3c267b898495c4
Badhon63 <jianrobiul7898@gmail.com>
2026-04-15 15:37:28 +0600

    added navbar

--- src/components/shared/navbar/Navbar.jsx
+++ src/components/shared/navbar/Navbar.jsx
@@ -3,23 +3,24 @@ import { FaHome, FaClock, FaChartPie } from "react-icons/fa";
 
 const Navbar = () => {
   const linkBase =
-    "flex items-center gap-2 px-5 py-2 rounded-full font-medium transition-all duration-200";
+    "flex items-center gap-2 px-4 py-2 rounded-md font-medium transition-all duration-200";
 
-  const activeStyle = "bg-green-700 text-white shadow-md";
-  const inactiveStyle = "text-gray-600 hover:bg-green-100";
+  const activeStyle = "bg-[#244D3F] text-white";
+  const inactiveStyle =
+    "text-[#244D3F] hover:bg-base-300";
 
   return (
-    <nav className="bg-white shadow-md sticky top-0 z-50">
+    <nav className="bg-white shadow-sm sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 md:px-8 py-4 flex justify-between items-center">
 
         {/* Logo */}
         <div className="text-2xl font-bold text-[#244D3F]">
-         <span className ="text-black">Keen </span>
-            Keeper
+          KeenKeeper
         </div>
 
-        {/* Links */}
+        {/* Right Side Links */}
         <div className="flex items-center gap-3">
+
           <NavLink
             to="/"
             className={({ isActive }) =>
@@ -49,6 +50,7 @@ const Navbar = () => {
             <FaChartPie />
             Stats
           </NavLink>
+
         </div>
       </div>
     </nav>
